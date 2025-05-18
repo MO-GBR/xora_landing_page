ee78f1d684619e24ba9c79f1c6cb29aa7e37fae8
Mohamed Gabr <70921709+MO-GBR@users.noreply.github.com>
2025-05-18 22:30:09 +0300

    Update Header.jsx

--- src/Sections/Header.jsx
+++ src/Sections/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
             <a className='lg:hidden flex-1 cursor-pointer z-2'>
                 <img src="/images/xora.svg" width={115} height={55} alt="logo" />
             </a>
-            <div className={`w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-[#3C52D9] max-lg:opacity-0 ${isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"}`}>
+            <div className={`w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:opacity-0 ${isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"}`}>
                 <div className='max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4'>
                     <nav className='"max-lg:relative max-lg:z-2 max-lg:my-auto max-lg:w-[60%]'>
                         <ul className='flex max-lg:block max-lg:px-12'>
